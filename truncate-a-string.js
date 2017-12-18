b1832a681bb7b5a0fd979c052031d1fe314ee49e
Juan Montoya <jcmontoya@gmail.com>
2017-12-18 20:56:13 +0000

    completed freeCodeCamp basic algorithm - Truncate a string

---
+++ truncate-a-string.js
@@ -0,0 +1,17 @@
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+   
+
+  if(str > num){
+   return str.slice(0, num-3)+'...';
+  }else if(num <= 3){
+   return str.slice(0, num) + '...';
+  }else if(str.length > num){
+   return str.slice(0, num-3)+'...';
+  }else{
+    return str;
+  }
+}
+
+
+truncateString("A-tisket a-tasket A green and yellow basket", 11);
