160b0791ba113ddc02e20a1e2e5733dbd9ee81d1
Juan Montoya <jcmontoya@gmail.com>
2017-12-19 04:06:27 +0000

    freeCodeCamp sum all primes completed

---
+++ sum-all-primes.js
@@ -0,0 +1,20 @@
+function isPrime(num){
+  for (var n = 2; n < num; n++) {
+    if (num % n === 0){
+      return false;        
+        }
+  }
+  return true;
+}
+
+function sumPrimes(num) {
+  var total = 2;
+  for (var n = 3; n <= num; n++){
+    if(isPrime(n)){
+      total += n;
+    }
+  }
+  return total;
+}
+
+sumPrimes(10);
