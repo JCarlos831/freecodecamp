3246455f94195c45576a2eb83f1819899c23449e
Juan Montoya <jcmontoya@gmail.com>
2018-03-27 14:26:05 +0000

    Moved reverseAString into new folder and commenced work on Chunky Monkey.  So far test cases 1, 2, and 4 pass.

---
+++ chunky-monkey.js
@@ -0,0 +1,24 @@
+function chunkArrayInGroups(arr, size) {
+  // Break it up.
+  var newArray = [];
+  var a = arr.slice(0,size);
+  var b = arr.slice(size, (size+size));
+
+  newArray.push(a);
+  newArray.push(b);
+  return newArray;  
+}
+
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3);
+
+
+
+// Additional test cases:
+
+// chunkArrayInGroups(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2) should return [[0, 1], [2, 3], [4, 5]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) should return [[0, 1, 2, 3], [4, 5]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3) should return [[0, 1, 2], [3, 4, 5], [6]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]].
